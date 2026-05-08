91ddb75688912dc5c3b35d55ca8ecab880085ac0
aayushsingh3710 <aayushsingh3720@gmail.com>
2026-05-08 16:54:28 +0530

    Correct prime number check and input handling
    Fix prime number checking logic and correct input prompt.

--- frontpage.py
+++ frontpage.py
@@ -4,4 +4,13 @@ b=0
 for i in name:
   b=b+1
 print("@",name,b)
-
+# tell about prime nature
+a=int(input("Enter a No:"))
+b=0
+for i in ranfe(1,a+1):
+  if a%i==0:
+    b=b+1
+  if b==2:
+    print("It is a Prime no")
+  else:
+    print("It is not a Prime no")
